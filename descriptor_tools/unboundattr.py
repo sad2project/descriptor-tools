1397543094ee157c60dfc4b18f52d2bdcf2c65f4
Jacob Zimmerman <jzimmerman@collaborative.com>
2016-02-18 16:16:03 -0600

    some documentation for UnboundAttribute

--- descriptor_tools/unboundattr.py
+++ descriptor_tools/unboundattr.py
@@ -8,8 +8,44 @@ DEFAULT = object()
 
 
 class UnboundAttribute:
-    # TODO: change to SetOnceAttribute
+    """
+    An UnboundAttribute is a similar concept to an unbound method. An unbound
+    attribute is a callable that only needs an instance from which to pull
+    the value from. They are generally produced by "binding" descriptors,
+    although alternative implementations could be made.
+
+    For instance, if a class, `Class` has a binding descriptor called `attr`,
+    you can get an unbound attribute with `Class.attr`. Then call it with any
+    instance of `Class` to get the value stored there.
+
+        inst1 = Class()
+        inst1.attr = 5
+        inst2 = Class()
+        inst2.attr = 4
+
+        attr = Class.attr
+
+        attr(inst1)
+        >> 5
+        attr(inst2)
+        >> 4
+
+    UnboundAttributes work especially well for providing simple keys for
+    comparison and for mapping to an attribute.
+
+        sorted_vals = vals.sort(key=Class.attr)
+        mapped_vals = map(Class.attr, vals)
+
+    UnboundAttributes can also be used to set and delete attributes on
+    instances using the `set()` and `delete()` methods.
+    """
     def __init__(self, descriptor, owner):
+        """
+
+        :param descriptor:
+        :param owner:
+        :return:
+        """
         self.descriptor = descriptor
         self.owner = owner
 
