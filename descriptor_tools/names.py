e3021068e522105374d9a1ab10cec6534d9a86a4
Jacob Zimmerman <sad2project@users.noreply.github.com>
2016-03-18 21:35:29 -0500

    Realigned some docs

--- descriptor_tools/names.py
+++ descriptor_tools/names.py
@@ -7,6 +7,7 @@ def name_of(descriptor, owner):
     the name of the attribute the descriptor is stored under.
     Also works if the given class is a subclass of the class that *actually*
     has the descriptor attribute
+    
     :param descriptor: descriptor the name is being looked up for
     :param owner: class that "owns" the descriptor
     :return: the name the descriptor is stored under on *owner*
@@ -23,9 +24,10 @@ def id_name_of(descriptor):
     """
     Returns a string of the hexidecimal version of *descriptor*'s id,
     sans the leading '0'. So, it'll be something like 'xf8e8aa97'. It
-	removes the 0 so that it will start with an alpha character,
-	allowing it to still be a proper Python identifier, which keeps it
-	from breaking `dir()`
+    removes the 0 so that it will start with an alpha character,
+    allowing it to still be a proper Python identifier, which keeps it
+    from breaking `dir()`
+    
     :param descriptor: descriptor to generate the name for/from
     :return: a generated name for the given descriptor
     """
