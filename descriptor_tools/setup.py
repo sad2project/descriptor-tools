bda4c9f79c888ce31726d903a373f93c9be8f646
Jacob <sad2project@gmail.com>
2016-03-25 16:43:34 -0500

    added setup info for pypi

---
+++ descriptor_tools/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'descriptor-tools',
+  packages = ['descriptor-tools'],
+  version = '1.0',
+  description = 'A collection of classes and functions to make the creation of descriptors simpler and quicker',
+  author = 'Jacob Zimmerman',
+  author_email = 'sad2project@gmail.com',
+  url = 'https://github.com/sad2project/descriptor-tools',
+  download_url = 'https://github.com/sad2project/descriptor-tools/tarball/1.0',
+  keywords = ['descriptors'],
+  classifiers = [],
+)
