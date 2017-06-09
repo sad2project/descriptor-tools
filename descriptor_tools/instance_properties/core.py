a31c032d0608e735814b1b8e065fea7cb228cdbf
Jacob <sad2project@gmail.com>
2017-06-09 11:49:19 -0500

    Finished instance property core code, as well as code docs
    added notes for a document on changing InstanceProperty

--- descriptor_tools/instance_properties/core.py
+++ descriptor_tools/instance_properties/core.py
@@ -1,5 +1,9 @@
 # coding=utf-8
+from abc import ABCMeta, abstractmethod
 from descriptor_tools import name_of, binding
+
+
+__all__ = ['InstanceProperty', 'by', 'DelegatedProperty']
 
 
 class OnInstanceStorageStrategy:
@@ -30,17 +34,13 @@ class InstanceProperty:
     class as `foo`, the delegated property would be stored on the instance under
     `_foo`.
 
-    If you would prefer to use a different name, you could create a subclass of
-    :InstanceProperty and override :__set_name.
-
-    If you would prefer to store the delegated property in a completely
-    different way, you should create a delegated property storage strategy class
-    and pass in an instance to the keyword argument `delegate_storage`.
-
-    For more details about changing how :InstanceProperty works, check out the
-    `ChangingInstanceProperty` documentation.
+    For details about changing how :InstanceProperty stores delegated
+    properties, check out the `ChangingInstanceProperty` documentation.
     """
-    def __init__(self, instantiator, *, delegate_storage=OnInstanceStorageStrategy()):
+    def __init__(self,
+                 instantiator,
+                 *,
+                 delegate_storage=OnInstanceStorageStrategy()):
         """
         Requires a function that can take at least `instance` and `name` keyword
         arguments, if not more in order to create an instance of a delegated
@@ -61,9 +61,6 @@ class InstanceProperty:
         self._secret_name = None
         self._delegate_storage = delegate_storage
 
-    def _assign_delegate(self, delegprop, instance):
-        self._delegate_storage.assign(self, delegprop, instance)
-
     @binding
     def __get__(self, instance, owner):
         """
@@ -79,7 +76,11 @@ class InstanceProperty:
         except AttributeError:
             # if this happens, then the property has not yet been initialized on
             # the instance yet, which means we need to return an initializer
-            return InstancePropertyInitializer(self, instance, self._instantiator, self._name)
+            return InstancePropertyInitializer(
+                self,
+                instance,
+                self._instantiator,
+                self._name)
         return delegprop.get()
 
     def __set__(self, instance, value):
@@ -90,8 +91,9 @@ class InstanceProperty:
             # The property doesn't implement `set()`, so the property is
             # considered read-only .
             raise AttributeError(
-                "Cannot set new value on read-only attribute, '{}'".format(self._name)
-                ) from e
+                    "Cannot set new value on read-only attribute, " +
+                    "'{}'".format(self._name)
+                    ) from e
 
     def _set_name(self, owner):
         if self._name is None:
@@ -103,10 +105,27 @@ class InstanceProperty:
 
 
 def by(delegated_property_instantiation_function):
+    """
+    A shortcut to creating an :InstanceProperty instance, :by uses the same
+    wording as the language that inspired this feature, Kotlin. If you do any of
+    the customization options of :InstanceProperty, it is recommended that you
+    make your own shortcut function like this one to make it easier to access
+    those customized versions.
+
+    :param delegated_property_instantiation_function: a function that
+    instantiates a delegated property, just like the kind :InstanceProperty needs
+    :return: an :InstanceProperty that is created using
+    :delegated_property_instantiation_function
+    """
     return InstanceProperty(delegated_property_instantiation_function)
 
 
 class InstancePropertyInitializer:
+    """
+    A tightly-coupled helper class that works with :InstanceProperty. Do not
+    touch. Do not create your own unless you are creating your own
+    instance property wrapper class that will work with this the same way.
+    """
     def __init__(self, instpropdesc, instance, instantiator, name):
         self.instpropdesc = instpropdesc
         self.instantiator = instantiator
@@ -114,5 +133,85 @@ class InstancePropertyInitializer:
         self.name = name
 
     def initialize(self, *args, **kwargs):
-        delegprop = self.instantiator(*args, **kwargs, instance=self.instance, name=self.name)
-        self.instpropdesc._assign_delegate(delegprop, self.instance)
+        delegprop = self.instantiator(
+                *args, **kwargs,
+                instance=self.instance,
+                name=self.name)
+        self.instpropdesc._delegate_storage.assign(
+                self.instpropdesc,
+                delegprop,
+                self.instance)
+
+
+class DelegatedProperty(metaclass=ABCMeta):
+    """
+    A Delegated Property is the kind of property you always wanted to write; it
+    only knows about and deals with a single instance of the class while
+    :InstanceProperty deals with all the details that were always so difficult
+    with descriptors.
+
+    It is not required to subclass :DelegatedProperty to create one, but it does
+    help label the class for what it is.
+
+    To create a Delegated Property, there at least 2 methods that you need:
+    `__init__()`
+    `get()`
+    If you want the Delegated Property to not be read-only, you will also need
+    `set()`.
+
+    Implementing `__init__()`:
+    `__init__()` will have 2 parameters, likely a 3rd, and possibly more. The
+    two gauranteed parameters are `instance` and `name`, which will be provided
+    as keyword arguments, so if you don't need them, you can simply end your
+    `__init__()` signature with `**kwargs` to swallow them up. If you don't just
+    swallow them up, make them the last parameters. If you deem it appropriate
+    to store `instance` on your delegated property, it is recommended that you
+    do so using a weak reference to it to prevent circular dependencies.
+
+    The likely third parameter is either the value the property is being
+    initialized with or something that you can derive the initial value from.
+    There are instances where not having an initial value is the whole point
+    (see `LateInit`).
+
+    In some rare cases, it's possible your Delegated Property will need even
+    more parameters in order to be properly initialized. If this is the case,
+    simply require them.
+
+    Users using your specific Delegated Property are the ones required to
+    provide all the arguments it needs, except `instance` and `name`, so just be
+    certain to provide a way to deal with those two, and the rest can be done
+    however you like.
+
+    `self` was not mentioned, but it still needs to be the first parameter.
+
+    Implementing `get()`:
+    `get()` is a very simple method; the only parameter is `self`, and its only
+    duty is to return the current value of the property.
+
+    Implementing `set()`:
+    If you want the Delegated Property to be read-only, simply ignore
+    implementing `set()`. Otherwise, know that, other than `self`, it has only
+    the one other parameter, `value`, which is the new value to set the property
+    to.
+
+    Final Implementation Notes:
+    It is recommended that the value of the property be stored on the Delegated
+    Property itself, rather than trying to find a place on `instance` to store
+    it. Saving on `instance` always has the chance that you'll use a clashing
+    name as another attribute.
+
+    Do not attempt to create a read-only and non-read-only version of a
+    Delegated Property, or to use a read-only flag in the class which can be
+    used to determine if it wants to be read-only. Rather, use the `ReadOnly`
+    decorator Delegated Property to make it effectively read-only.
+
+    Any Delegated Property instance that needs to use `set()`, even though it's
+    supposed to be read-only (i.e. `LateInit`), cannot use the `ReadOnly`
+    decorator and will have to implement being read-only in their own way.
+    """
+    @abstractmethod
+    def get(self):
+        ...
+
+    def set(self, value):
+        raise AttributeError
