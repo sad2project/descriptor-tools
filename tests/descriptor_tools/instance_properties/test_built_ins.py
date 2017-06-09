6974ffd6ac27cca7f8a6258fa0a35f1b379e1796
Jacob <sad2project@gmail.com>
2017-06-09 12:04:59 -0500

    Added failing stub test as a reminder to implement the tests

---
+++ tests/descriptor_tools/instance_properties/test_built_ins.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+
+class TODOTest(TestCase):
+    def test_still_need_to_do_tests(self):
+        self.fail("Tests for built-in delegated properties still need to be made")
